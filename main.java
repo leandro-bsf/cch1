05a9ea7dd13d170353cba102efb696e9b16324d5
leandro-bsf <motorola1710@gmail.com>
2023-05-17 22:46:53 -0300

    alteracao17-05

--- main.java
+++ main.java
@@ -2,20 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class main {
-    public static void main(String[] args) throws CloneNotSupportedException {
-       
-        
+public static void main(String[] args) throws CloneNotSupportedException {
         Contact  leandro = new Contact("leandro", "4691525555", "momdoemdoedme");
-        
         leandro.imprimir();
         Contact mateus = (Contact) leandro.clone(); 
+        System.out.println("Imprimindo objeto  "+mateus.getNome());
+        mateus.imprimir();
+        System.out.println("alterando atributos do objeto clonado");
         mateus.setNome("Mateus");
         mateus.setEmail("batatinha123@gmail");
         mateus.imprimir();
-
-        System.out.println("Salvando contato na lista");   
-        
- 
-        
+            
     }
 }
